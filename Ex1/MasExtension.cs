0d09eeb0cdd82af492d6aede23c8442efced7f7a
Федина Юлия <yu.belova@2gis.ru>
2019-08-07 09:56:40 +0700

    Задание для 11 лекции

---
+++ Ex1/MasExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex1
+{
+    public static class MasExtension
+    {
+        public static void OutputMas(this int[] mas)
+        {
+            foreach (var item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static string MasThroughSeparator(this int[] mas, string separator)
+        {
+            var result="";
+            foreach (var item in mas)
+            {
+                result += ($"{item}{separator}");
+            }
+
+            return result;
+        }
+    }
+}
--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -4,9 +4,25 @@ namespace Ex1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //Написать метод-расширение для массива целых чисел, который выводит этот массив на консоль.
+            var array = new int[10];
+            var rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100);
+            }
+
+            MasExtension.OutputMas(array);
+
+            //Написать метод-расширение для массива целых чисел,
+            //который принимает как параметр строку-разделитель,
+            //а возвращает строку, в которой содержатся все элементы массива перечисленные через указанный разделитель.
+
+            string result = MasExtension.MasThroughSeparator(array, "|");
+            Console.WriteLine(result);
         }
+
     }
 }
