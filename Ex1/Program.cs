197a0e0bb99232e0db1dda2d4829151e9a2db7ed
Федина Юлия <yu.belova@2gis.ru>
2019-08-21 12:57:52 +0700

    Поправила замечания

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -9,7 +9,7 @@ namespace Ex1
             //Написать метод-расширение для массива целых чисел, который выводит этот массив на консоль.
             var array = new int[10];
             var rnd = new Random();
-            for (int i = 0; i < array.Length; i++)
+            for (var i = 0; i < array.Length; i++)
             {
                 array[i] = rnd.Next(100);
             }
--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -12,13 +12,12 @@ namespace Ex2
 
             var array = new int[10];
             var rnd = new Random();
-            for (int i = 0; i < array.Length; i++)
+            for (var i = 0; i < array.Length; i++)
             {
                 array[i] = rnd.Next(100);
             }
 
-            var result = MasExtension.MasThroughSeparator(array, "|");
-            Console.WriteLine(result);
+            Console.WriteLine(array.MasThroughSeparator("|"));
         }
     }
 }
